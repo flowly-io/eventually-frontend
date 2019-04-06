0a87dff89745ec8f067adfe381d8e1f5713182a9
Andrey Mayorov <a.d.mayorov@gmail.com>
2019-04-06 17:24:03 +1100

    Cosmetic fixes

--- src/components/ViewCard.js
+++ src/components/ViewCard.js
@@ -8,48 +8,36 @@ import CardHeader from "@material-ui/core/CardHeader"
 import Checkbox from '@material-ui/core/Checkbox';
 import Edit from "@material-ui/icons/Edit";
 import { Table, TableRow, TableCell } from '@material-ui/core'
-import { defaultProps } from "recompose";
-
-function CapabilityRow(props) {
-  return (
-    <div>
-      <Checkbox
-        label="test" key="0"
-        onChange={() => console.log("chek")}
-      />
-      <Typography>Capability goes here</Typography>
-    </div>
-  )
-}
 
 function CapabilityGroup(props) {
   return (
-    
-  props.caps.map(cap => {return (
-    <div>
-      <Typography variant="h6">
-        {cap.name}
-      </Typography>
-      <CapabilityTable checks={cap.checkpoints} />
-    </div>
-  )})
+    props.caps.map(cap => {
+      return (
+        <div>
+          <Typography variant="h6">
+            {cap.name}
+          </Typography>
+          <CapabilityTable checks={cap.checkpoints} />
+        </div>
+      )
+    })
   )
 }
 
 function CapabilityTable(props) {
   return (
-      props.checks.map((chk, i) => {
-        return (
-          <TableRow>
-            <TableCell padding="checkbox">
-              <Checkbox />
-            </TableCell>
-            <TableCell key={i} >
-              {chk.description}
-            </TableCell>
-          </TableRow>
-        );
-      })
+    props.checks.map((chk, i) => {
+      return (
+        <TableRow>
+          <TableCell padding="checkbox">
+            <Checkbox />
+          </TableCell>
+          <TableCell key={i} >
+            {chk.description}
+          </TableCell>
+        </TableRow>
+      );
+    })
   )
 }
 
@@ -62,10 +50,8 @@ function ViewCard(props) {
           <Typography variant="h5" color="textSecondary">
             {props.event.startDateTime} - {props.event.endDateTime}
           </Typography>
-
         </CardContent>
         <CardContent>
-
           <CapabilityGroup caps={props.event.capabilities} />
         </CardContent>
       </Card>
