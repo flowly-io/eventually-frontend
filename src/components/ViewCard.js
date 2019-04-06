45a7a49a369867c741e3337026d51d237af49d48
Andrey Mayorov <a.d.mayorov@gmail.com>
2019-04-06 17:13:51 +1100

    Capabilites and checkpoints listed in view event page

--- src/components/ViewCard.js
+++ src/components/ViewCard.js
@@ -5,27 +5,72 @@ import CardActions from "@material-ui/core/CardActions";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import CardHeader from "@material-ui/core/CardHeader"
+import Checkbox from '@material-ui/core/Checkbox';
 import Edit from "@material-ui/icons/Edit";
+import { Table, TableRow, TableCell } from '@material-ui/core'
+import { defaultProps } from "recompose";
+
+function CapabilityRow(props) {
+  return (
+    <div>
+      <Checkbox
+        label="test" key="0"
+        onChange={() => console.log("chek")}
+      />
+      <Typography>Capability goes here</Typography>
+    </div>
+  )
+}
+
+function CapabilityGroup(props) {
+  return (
+    
+  props.caps.map(cap => {return (
+    <div>
+      <Typography variant="h6">
+        {cap.name}
+      </Typography>
+      <CapabilityTable checks={cap.checkpoints} />
+    </div>
+  )})
+  )
+}
+
+function CapabilityTable(props) {
+  return (
+      props.checks.map((chk, i) => {
+        return (
+          <TableRow>
+            <TableCell padding="checkbox">
+              <Checkbox />
+            </TableCell>
+            <TableCell key={i} >
+              {chk.description}
+            </TableCell>
+          </TableRow>
+        );
+      })
+  )
+}
 
 function ViewCard(props) {
-    return (
-      <div style={{padding: 100}}>
-        <Card>
-            <CardHeader variant="h1" title={props.event.name}/>
-            <CardContent>
-                <Typography variant="h5" color="textSecondary">
-                    {props.event.startDateTime} - {props.event.endDateTime}
-                </Typography>
+  return (
+    <div style={{ padding: 100 }}>
+      <Card>
+        <CardHeader variant="h1" title={props.event.name} />
+        <CardContent>
+          <Typography variant="h5" color="textSecondary">
+            {props.event.startDateTime} - {props.event.endDateTime}
+          </Typography>
 
-            </CardContent>
-            <CardContent>
-              <Typography variant="h6">
-                Capabilities:
-              </Typography>
-            </CardContent>
-        </Card>
-      </div>
-    );
+        </CardContent>
+        <CardContent>
+
+          <CapabilityGroup caps={props.event.capabilities} />
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
 
 export default ViewCard;
