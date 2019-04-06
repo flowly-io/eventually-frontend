a85b7c8080a945873749067002518a07f9017469
Andrey Mayorov <a.d.mayorov@gmail.com>
2019-04-06 14:47:20 +1100

    Added ViewEvent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Route, Link, Switch } from "react-router-dom";
 
-import { AppBar, Typography, Toolbar, Button} from "@material-ui/core";
+import { AppBar, Typography, Toolbar } from "@material-ui/core";
 
 import CreateEvent from "./pages/Create";
+import ViewEvent from "./pages/View";
 
 function Home() {
   return <h2>Home</h2>;
@@ -17,24 +18,25 @@ function AppRouter() {
           <Typography variant="h3" style={{ color: "white", flexGrow: 1 }}>
             eVentually
           </Typography>
-          <Button component={Link}
+          <Link
             to="/"
             style={{ fontWeight: 700, paddingLeft: 16, color: "white" }}
           >
             Home
-          </Button>
+          </Link>
 
-          <Button component={Link}
+          <Link
             to="/create"
             style={{ fontWeight: 700, paddingLeft: 16, color: "white" }}
           >
             Create
-          </Button>
+          </Link>
         </Toolbar>
       </AppBar>
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/create" component={CreateEvent} />
+        <Route path="/view" component={ViewEvent} />
       </Switch>
     </div>
   );
---
+++ src/pages/View.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { TextField, Card, CardContent, CardHeader } from "@material-ui/core";
+import { DateTimePicker } from "material-ui-pickers";
+import moment from "moment";
+
+class ViewEvent extends React.Component {
+  state = {
+    name: "",
+    fromDateTime: new moment(),
+    toDateTime: new moment()
+  };
+  render() {
+    const { name, fromDateTime, toDateTime } = this.state;
+    const eventDuration = toDateTime.diff(fromDateTime, "hours");
+    return (
+      <div style={{ padding: 10 }}>
+        <Card>
+          <CardHeader title="Create an event" />
+          <CardContent>
+            <TextField
+              id="event-name"
+              label="Name"
+              value={name}
+              onChange={e => {
+                this.setState({
+                  name: e.target.value
+                });
+              }}
+              margin="normal"
+              style={{ width: "100%" }}
+            />
+            <DateTimePicker
+              value={fromDateTime}
+              disablePast
+              onChange={val =>
+                this.setState({ fromDateTime: val, toDateTime: val })
+              }
+              label="Event Start"
+              showTodayButton
+            />
+            <DateTimePicker
+              value={toDateTime}
+              disablePast
+              onChange={val => this.setState({ toDateTime: val })}
+              label="Event End"
+              minDate={fromDateTime}
+              showTodayButton
+            />
+            {`Event Duration: ${eventDuration} hours`}
+          </CardContent>
+        </Card>
+        <div />
+      </div>
+    );
+  }
+}
+
+export default ViewEvent;
